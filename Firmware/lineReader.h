f8045d820f383c252fffed3effca6801a4f2c213
gustavosdepaula <sdepaula.gustavo@gmail.com>
2017-06-10 10:53:56 -0300

    Update for Mbed

--- Firmware/lineReader.h
+++ Firmware/lineReader.h
@@ -1,14 +1,38 @@
+#ifndef LINE_READER_H
+#define LINE_READER_H
+
+#include "mbed.h"
 #include "_config.h"
 #include "_types.h"
 
-#ifndef MUX_H
-#define MUX_H
+//Instanciando 
+AnalogIn inLR_S0(PIN_LR_S0);
+AnalogIn inLR_S1(PIN_LR_S1);
+AnalogIn inLR_S2(PIN_LR_S2);
+AnalogIn inLR_S3(PIN_LR_S3);
+AnalogIn inLR_S4(PIN_LR_S4);
+AnalogIn inLR_S5(PIN_LR_S5);
+DigitalIn inMarkingL(PIN_LR_S0);
+DigitalIn inMarkingR(PIN_LR_S0);
 
 class LineReader{
 public:
-  static void init();
+	//LineReader(PinName pin);
+	static void init();
   static void readValues();
   static int getValue(int index);
   static float getPosition();
+
+private:
+/*AnalogIn 			inLR_S0;
+	AnalogIn 			inLR_S1;
+	AnalogIn 			inLR_S2;
+	AnalogIn 			inLR_S3;
+	AnalogIn 			inLR_S4;
+	AnalogIn 			inLR_S5;
+	DigitalIn 		inMarkingL;
+	DigitalIn 		inMarkingR;
+*/
 };
+
 #endif
