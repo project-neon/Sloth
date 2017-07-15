c964d7c5b98ffa5f5d77eee2e26cfbf78274b557
gustavosdepaula <sdepaula.gustavo@gmail.com>
2017-07-15 12:17:33 -0300

    Hello World

---
+++ Firmware/Sloth_v1.5/lib/Motor/Motor.cpp
@@ -0,0 +1,23 @@
+#include "Motor.h"
+
+Motor::Motor(PinName pwm, PinName in1, PinName in2):
+        _pwm(pwm), _in1(in1), _in2(in2)  {
+    _pwm.period(0.001); //001
+    _pwm = 0;
+}
+
+void Motor::speed(float speed) {
+    _pwm = speed > 0 ? speed : -speed;
+    _in1 = (speed >=  0 ? 1 : 0);
+    _in2 = (speed >   0 ? 0 : 1);
+}
+
+void Motor::coast() {
+    _in1 = 0;
+    _in2 = 0;
+}
+
+void Motor::brake() {
+    _in1 = 1;
+    _in2 = 1;
+}
