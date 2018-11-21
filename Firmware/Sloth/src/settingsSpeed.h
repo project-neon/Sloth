65632bd3be63d576916858f0755a45740e336492
marcelofariaz <Marcelo Farias>
2018-11-20 23:37:59 -0200

    Create a robot test code to Maple Mini running on Arduino IDE

--- Firmware/Sloth/src/settingsSpeed.h
+++ Firmware/Sloth/src/settingsSpeed.h
@@ -4,12 +4,13 @@
 #ifndef SETTINGSSPEED_H
 #define SETTINGSSPEED_H
 
-Setup k0[] { // SlowCurve,Curve, FastCurve, Straight, Stop
-  {0.62, 0.00025, 0.000000, 0.0000050},
-  {0.60, 0.00022, 0.000000, 0.0000050},
-  {0.70, 0.00022, 0.000000, 0.0000075},
-  {1.00, 0.00018, 0.000000, 0.0000075},
-  {0.10, 0.00010, 0.000000, 0.0000010}
+Setup k0[] {
+  // Target Speed, kp, ki, kd
+  {0.62, 0.00025, 0.000000, 0.0000050}, // Slow Curve
+  {0.60, 0.00022, 0.000000, 0.0000050}, // Curve
+  {0.70, 0.00022, 0.000000, 0.0000075}, // FastCurve
+  {1.00, 0.00018, 0.000000, 0.0000075}, // Straight
+  {0.10, 0.00010, 0.000000, 0.0000010}  // Stop
 };
 
 Setup k1[] { // SlowCurve,Curve, FastCurve, Straight, Stop
